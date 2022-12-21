2ec3aca94a7703cc5aca00538f424ff0e908b3e5
Luis Gutiérrez <luisghtz@outlook.com>
2022-12-20 18:20:11 -0600

    Fix end line error

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    // https://stackoverflow.com/questions/70473135/delete-eslintprettier-prettier
+    "prettier/prettier": ["error", { "endOfLine": "auto" }]
   },
 };
