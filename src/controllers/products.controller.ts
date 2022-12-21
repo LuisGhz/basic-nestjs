a5b831f450db84af2511e2077490e8b52201331c
Luis Gutiérrez <luisghtz@outlook.com>
2022-12-21 16:50:53 -0600

    Handle Response

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,10 +1,20 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  HttpStatus,
+  HttpCode,
+  Res,
+} from '@nestjs/common';
 import {
   Delete,
   Post,
   Put,
 } from '@nestjs/common/decorators/http/request-mapping.decorator';
 import { Body } from '@nestjs/common/decorators/http/route-params.decorator';
+
+import { Response } from 'express';
 
 @Controller('products')
 export class ProductsController {
@@ -17,14 +27,17 @@ export class ProductsController {
   getMany(@Query('limit') limit: number, @Query('offset') offset = 0) {
     return `limit: ${limit}, offset: ${offset}`;
   }
-  @Get('/:id')
-  getOne(@Param() params: any) {
-    return `Product: ${params.id}`;
-  }
   // @Get('/:id')
-  // getOne(@Param('id') id: string) {
-  //   return `Product: ${id}`
+  // @HttpCode(HttpStatus.ACCEPTED)
+  // getOne(@Param() params: any) {
+  //   return `Product: ${params.id}`;
   // }
+  @Get('/:id')
+  getOne(@Res() response: Response, @Param('id') id: string) {
+    response.status(200).send({
+      message: `Product: ${id}`,
+    });
+  }
   @Post()
   create(@Body() payload: any) {
     return {
