33f9365c0da8ead66040249c7944f7099b367ee1
Luis Gutiérrez <luisghtz@outlook.com>
2022-12-21 14:13:58 -0600

    Create products controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+// Controllers
+import { AppController } from './app.controller';
+import { ProductsController } from './controllers/products/products.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ProductsController],
   providers: [AppService],
 })
 export class AppModule {}
