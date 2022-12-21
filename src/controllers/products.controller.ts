f40af1d9f8b6023b5968d48e2029784adf36732f
Luis Gutiérrez <luisghtz@outlook.com>
2022-12-21 14:26:47 -0600

    Rename methods

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -3,20 +3,20 @@ import { Controller, Get, Param, Query } from '@nestjs/common';
 @Controller('products')
 export class ProductsController {
   // @Get('list')
-  // getProducts(@Query() params: any) {
+  // getMany(@Query() params: any) {
   //   const { limit, offset = 0 } = params;
   //   return `limit: ${limit}, offset: ${offset}`;
   // }
   @Get('list')
-  getProducts(@Query('limit') limit: number, @Query('offset') offset = 0) {
+  getMany(@Query('limit') limit: number, @Query('offset') offset = 0) {
     return `limit: ${limit}, offset: ${offset}`;
   }
   @Get('/:id')
-  getProduct(@Param() params: any) {
+  getOne(@Param() params: any) {
     return `Product: ${params.id}`;
   }
   // @Get('/:id')
-  // getProduct(@Param('id') id: string) {
+  // getOne(@Param('id') id: string) {
   //   return `Product: ${id}`
   // }
 }
