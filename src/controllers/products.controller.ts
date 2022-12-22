cca478340ba7fdfb45a551f0a4986f84bc5b617c
Luis Gutiérrez <luisghtz@outlook.com>
2022-12-22 10:12:32 -0600

    Create CRUD

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -15,29 +15,30 @@ import {
 import { Body } from '@nestjs/common/decorators/http/route-params.decorator';
 
 import { Response } from 'express';
+import { Product } from 'src/entities/product.entity';
+import { ProductsService } from 'src/services/products.service';
 
 @Controller('products')
 export class ProductsController {
+  constructor(private _productsService: ProductsService) {}
   // @Get('list')
   // getMany(@Query() params: any) {
   //   const { limit, offset = 0 } = params;
   //   return `limit: ${limit}, offset: ${offset}`;
   // }
   @Get('list')
-  getMany(
-    @Res() response: Response,
-    @Query('limit') limit: number,
-    @Query('offset') offset = 0,
-  ) {
-    response.status(202).send({
-      limit,
-      offset,
-    });
+  getMany(@Query('limit') limit: number, @Query('offset') offset = 0) {
+    // response.status(202).send({
+    //   limit,
+    //   offset,
+    // });
+    return this._productsService.findAll();
   }
   @Get('/:id')
   @HttpCode(HttpStatus.ACCEPTED)
-  getOne(@Param() params: any) {
-    return `Product: ${params.id}`;
+  getOne(@Param('id') id: number) {
+    // return `Product: ${id}`;
+    return this._productsService.findOne(id);
   }
   // @Get('/:id')
   // getOne(@Res() response: Response, @Param('id') id: string) {
@@ -46,24 +47,23 @@ export class ProductsController {
   //   });
   // }
   @Post()
-  create(@Body() payload: any) {
+  create(@Body() payload: Product) {
+    const newProduct = this._productsService.create(payload);
     return {
-      message: 'Message',
-      payload,
+      message: 'Created',
+      payload: {
+        ...newProduct,
+      },
     };
   }
   @Put(':id')
-  update(@Param('id') id: number, @Body() payload: any) {
-    return {
-      id,
-      payload,
-    };
+  update(@Param('id') id: number, @Body() payload: Product) {
+    return this._productsService.update(id, payload);
   }
 
   @Delete(':id')
   delete(@Param('id') id: number) {
-    return {
-      id,
-    };
+    this._productsService.delete(id);
+    return true;
   }
 }
--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,4 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Product } from 'src/entities/product.entity';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  #counter = 3;
+  #products: Product[] = [
+    {
+      id: 1,
+      name: 'Product 1',
+      description: 'Desc 1',
+      price: 50,
+      stock: 100,
+      image: '',
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      description: 'Desc 2',
+      price: 60,
+      stock: 120,
+      image: '',
+    },
+  ];
+
+  findAll() {
+    return this.#products;
+  }
+
+  findOne(id: number) {
+    const product = this.#products.find((p) => p.id === Number(id));
+    if (!product)
+      throw new NotFoundException(`Product with id ${id} does not exist`);
+    return product;
+  }
+
+  create(payload: Product) {
+    const newProduct: Product = {
+      id: this.#counter++,
+      ...payload,
+    };
+
+    this.#products.push(newProduct);
+    return this.#products[this.#products.length - 1];
+  }
+
+  update(id: number, payload: Product) {
+    const product = this.findOne(id);
+    if (!product) return null;
+
+    const index = this.#products.findIndex((p) => p.id === Number(id));
+    this.#products[index] = {
+      ...product,
+      ...payload,
+    };
+    return this.#products[index];
+  }
+
+  delete(id: number) {
+    const index = this.#products.findIndex((p) => p.id === Number(id));
+    if (index === -1)
+      throw new NotFoundException(`Product with id ${id} does not exist`);
+    this.#products.splice(index, 1);
+  }
+}
